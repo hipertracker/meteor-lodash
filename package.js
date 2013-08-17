1b461ee339309300790820c742bd242020e342d7
Jaroslaw Zabiello <hipertracker@gmail.com>
2013-08-17 12:12:16 +0100

    Updated to new Meteor 0.6.5 requirements

--- package.js
+++ package.js
@@ -1,9 +1,11 @@
 Package.describe({
-  summary: "Lo-Dash like Underscore.js but faster and more consistent"
+  summary: "A utility library delivering consistency, customization, performance, & extras. Can be a drop-in place replacement for Underscore.js."
 });
 
 Npm.depends({'lodash': '1.3.1'});
 
 Package.on_use(function (api) {
+  api.use('meteor', {unordered: true});
+  api.export('_');
   api.add_files(["lib/lodash.js"], ["client", "server"]);
 });
